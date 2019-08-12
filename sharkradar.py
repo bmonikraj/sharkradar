f3af295521068058a25967c36e6eb77ff49ab4df
Monik Raj Behera <bmonikraj@gmail.com>
2019-08-13 04:57:49 +0530

    gitignore

--- sharkradar.py
+++ sharkradar.py
@@ -2,5 +2,7 @@ from waitress import serve
 from main import app
 
 if __name__ == "__main__":
-    print("Starting Shark-Radar Service Registry and Discovery Server on port 16461 for all IP Addresses")
+    print("")
+    print("Starting Shark-Radar\nService Registry and Discovery Server\non port 16461 for all IP Addresses")
+    print("")
     serve(app, listen="*:16461")
