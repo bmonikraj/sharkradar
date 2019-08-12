84b7ad035f7c1bb2965c1b4b6f2bd490cc9c5f6d
Monik Raj Behera <bmonikraj@gmail.com>
2019-08-12 23:05:20 +0530

    autopep8

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import sharkradarDbutils
+from Discovery import Discovery
+from Health import Health
 import sys
 from os.path import dirname as opd, realpath as opr
 import os
@@ -8,39 +11,37 @@ from flask import Flask, request
 basedir = opd(opr(__file__))
 sys.path.append(basedir)
 
-from Health import Health
-from Discovery import Discovery
-import sharkradarDbutils
 
 sharkradarDbutils.createTableIfNotExists()
 app = Flask(__name__)
 
+
 @app.route("/health", methods=['PUT'])
 def health():
-	response_objects = {"status" : "False"}
-	json_object = {}
-	json_object['ip'] = request.form['ip']
-	json_object['port'] = request.form['port']
-	json_object['service_name'] = request.form['service_name']
-	json_object['status'] = request.form['status']
-	json_object['mem_usage'] = request.form['mem_usage']
-	json_object['cpu_usage'] = request.form['cpu_usage']
-	json_object['nw_tput_bw_ratio'] = request.form['nw_tput_bw_ratio']
-	json_object['req_active_ratio'] = request.form['req_active_ratio']
-	json_object['success_rate'] = request.form['success_rate']
-	json_object['health_interval'] = request.form['health_interval']
-	json_object['current_timestamp'] = time.time()
-	respBool = Health.health(json_object)
-	if respBool:
-		response_objects["status"] = "True"
-		return json.dumps(response_objects)
-	return json.dumps(response_objects)
+    response_objects = {"status": "False"}
+    json_object = {}
+    json_object['ip'] = request.form['ip']
+    json_object['port'] = request.form['port']
+    json_object['service_name'] = request.form['service_name']
+    json_object['status'] = request.form['status']
+    json_object['mem_usage'] = request.form['mem_usage']
+    json_object['cpu_usage'] = request.form['cpu_usage']
+    json_object['nw_tput_bw_ratio'] = request.form['nw_tput_bw_ratio']
+    json_object['req_active_ratio'] = request.form['req_active_ratio']
+    json_object['success_rate'] = request.form['success_rate']
+    json_object['health_interval'] = request.form['health_interval']
+    json_object['current_timestamp'] = time.time()
+    respBool = Health.health(json_object)
+    if respBool:
+        response_objects["status"] = "True"
+        return json.dumps(response_objects)
+    return json.dumps(response_objects)
 
-@app.route("/discovery/<service_name>", methods = ['GET'])
+
+@app.route("/discovery/<service_name>", methods=['GET'])
 def discovery(service_name):
-	response_objects = {"ip" : "", "port" : ""}
-	respTuple = Discovery.discovery(service_name)
-	response_objects["ip"] = respTuple[0]
-	response_objects["port"] = respTuple[1]
-	return json.dumps(response_objects)
-	
+    response_objects = {"ip": "", "port": ""}
+    respTuple = Discovery.discovery(service_name)
+    response_objects["ip"] = respTuple[0]
+    response_objects["port"] = respTuple[1]
+    return json.dumps(response_objects)
