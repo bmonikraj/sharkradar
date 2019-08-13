a1687e569538e8fdac46f4a8de17f4c31dbe09ab
Monik Raj Behera <bmonikraj@gmail.com>
2019-08-14 02:20:24 +0530

    packaging setup

--- main.py
+++ main.py
@@ -1,47 +1,31 @@
-import sharkradarDbutils
-from Discovery import Discovery
-from Health import Health
 import sys
 from os.path import dirname as opd, realpath as opr
 import os
-import time
-import json
-from flask import Flask, request
+from waitress import serve
 
 basedir = opd(opr(__file__))
 sys.path.append(basedir)
 
+from src.Controller.Controller import app
 
-sharkradarDbutils.createTableIfNotExists()
-app = Flask(__name__)
+def main():
+	try:
+		first_arg = sys.argv[1]
+		if first_arg.split("=")[0]!="--bind":
+			print("Command argument format : --bind=<IP-addr>:<port>")
+			sys.exit(20)
+		listen_arg = first_arg.split("=")[1]
+		if len(listen_arg.split(":"))!=2:
+			print("Command argument format : --bind=<IP-addr>:<port>")
+			sys.exit(30)
+		print("")
+		print("Starting Shark-Radar")
+		print("Service Registry and Discovery Server")
+		print("on [{0}] for all IP Addresses".format(listen_arg))
+		print("")
+		serve(app, listen=listen_arg)
+	except Exception as e:
+		print("Command argument format : --bind=<IP-addr>:<port>")
 
-
-@app.route("/health", methods=['PUT'])
-def health():
-    response_objects = {"status": "False"}
-    json_object = {}
-    json_object['ip'] = request.form['ip']
-    json_object['port'] = request.form['port']
-    json_object['service_name'] = request.form['service_name']
-    json_object['status'] = request.form['status']
-    json_object['mem_usage'] = request.form['mem_usage']
-    json_object['cpu_usage'] = request.form['cpu_usage']
-    json_object['nw_tput_bw_ratio'] = request.form['nw_tput_bw_ratio']
-    json_object['req_active_ratio'] = request.form['req_active_ratio']
-    json_object['success_rate'] = request.form['success_rate']
-    json_object['health_interval'] = request.form['health_interval']
-    json_object['current_timestamp'] = time.time()
-    respBool = Health.health(json_object)
-    if respBool:
-        response_objects["status"] = "True"
-        return json.dumps(response_objects)
-    return json.dumps(response_objects)
-
-
-@app.route("/discovery/<service_name>", methods=['GET'])
-def discovery(service_name):
-    response_objects = {"ip": "", "port": ""}
-    respTuple = Discovery.discovery(service_name)
-    response_objects["ip"] = respTuple[0]
-    response_objects["port"] = respTuple[1]
-    return json.dumps(response_objects)
+if __name__ == "__main__":
+	main()
--- sharkradar.py
+++
@@ -1,8 +0,0 @@
-from waitress import serve
-from main import app
-
-if __name__ == "__main__":
-    print("")
-    print("Starting Shark-Radar\nService Registry and Discovery Server\non port 16461 for all IP Addresses")
-    print("")
-    serve(app, listen="*:16461")
