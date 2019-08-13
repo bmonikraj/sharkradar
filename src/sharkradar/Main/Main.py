fa3d1898e90f50fa61c5e19fd04b14ca632e9071
Monik Raj Behera <bmonikraj@gmail.com>
2019-08-14 04:19:07 +0530

    proper error help

--- src/sharkradar/Main/Main.py
+++ src/sharkradar/Main/Main.py
@@ -8,24 +8,26 @@ sys.path.append(basedir)
 
 from sharkradar.Controller.Controller import app
 
+CLI_ARG_ERROR_HELP = "***\nCommand argument format : sharkradar --bind=<IP-addr>:<port>\n***"
+
 def main():
 	try:
 		first_arg = sys.argv[1]
 		if first_arg.split("=")[0]!="--bind":
-			print("Command argument format : --bind=<IP-addr>:<port>")
+			print(CLI_ARG_ERROR_HELP)
 			sys.exit(20)
 		listen_arg = first_arg.split("=")[1]
 		if len(listen_arg.split(":"))!=2:
-			print("Command argument format : --bind=<IP-addr>:<port>")
+			print(CLI_ARG_ERROR_HELP)
 			sys.exit(30)
 		print("")
 		print("Starting Shark-Radar")
 		print("Service Registry and Discovery Server")
-		print("on [{0}] for all IP Addresses".format(listen_arg))
+		print("on {0}".format(listen_arg))
 		print("")
 		serve(app, listen=listen_arg)
 	except Exception as e:
-		print("Command argument format : --bind=<IP-addr>:<port>")
+		print(CLI_ARG_ERROR_HELP)
 
 if __name__ == "__main__":
 	main()
